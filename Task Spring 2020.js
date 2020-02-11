ebad5ae9cc0d5fe37c2e25e48a19c0b71640258c
Denitsa Pambelova <34513613+DenitsaPambelova@users.noreply.github.com>
2020-02-11 11:24:46 +0200

    Update Task Spring 2020.js

--- Task Spring 2020.js
+++ Task Spring 2020.js
@@ -3,42 +3,40 @@ function MentorMateLogo(n) {
     let star = '*';
     let dash = "-";
 
-        let printLogo =()=>{
-            let firstPart = '';
-            for (let i = 0; i < (n + 1) / 2; i++) {
-                let left = dash.repeat(n - i);
-                let letter = star.repeat(n + 2 * i);
-                let middle = dash.repeat(n - 2 * i);
-                let betweenLetters = dash.repeat(2 * n - 2 * i);
-                firstPart = left + letter + middle + letter + betweenLetters + letter + middle + letter + left;
-                console.log(firstPart);
-            }
-            let secondPart = '';
-            for (let i = 0; i < (n + 1) / 2; i++) {
-                let left = dash.repeat(n / 2 - i);
-                let letter1 = star.repeat(n);
-                let middle = dash.repeat(1 + 2 * i);
-                let letter2 = star.repeat((2 * n - 1) - 2 * i);
-                let betWeenLetters = dash.repeat(n / 2 - i);
-                let full = left + letter1 + middle + letter2 + middle + letter1;
-                secondPart = full + betWeenLetters + full + left;
-                console.log(secondPart);
-        }
+    if(n< 3 || n > 9999) {
+        console.log("The input must be odd number between 2 and 10 000.")
+        return;
+    }
+    else if(Number(n) % 2 === 0) {
+        console.log("The number must be odd!")
+        return;
     }
 
+    let printLogo = () => {
+        let firstPart = '';
+        for (let i = 0; i < (n + 1) / 2; i++) {
+            let left = dash.repeat(n - i);
+            let letter = star.repeat(n + 2 * i);
+            let middle = dash.repeat(n - 2 * i);
+            let betweenLetters = dash.repeat(2 * n - 2 * i);
+            firstPart = left + letter + middle + letter + betweenLetters + letter + middle + letter + left;
+            console.log(firstPart);
+        }
+        let secondPart = '';
+        for (let i = 0; i < (n + 1) / 2; i++) {
+            let left = dash.repeat(n / 2 - i);
+            let letter1 = star.repeat(n);
+            let middle = dash.repeat(1 + 2 * i);
+            let letter2 = star.repeat((2 * n - 1) - 2 * i);
+            let betWeenLetters = dash.repeat(n / 2 - i);
+            let full = left + letter1 + middle + letter2 + middle + letter1;
+            secondPart = full + betWeenLetters + full + left;
+            console.log(secondPart);
+        }
+    }
     let MentorMateLogo = {
         printLogo: printLogo(),
     }
-
     return MentorMateLogo.printLogo;
-}
 
-MentorMateLogo(3);
-console.log()
-console.log()
-MentorMateLogo(5)
-console.log()
-console.log()
-MentorMateLogo(7);
-console.log()
-console.log()
+}
